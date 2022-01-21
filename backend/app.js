b87d81d0d1374268c7142d6e36dd3d36a5005a67
Jesse <jesse.movecar@gmail.com>
2022-01-21 16:33:13 +0100

    Mise en place du backend

---
+++ backend/app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+// const mongoose = require('mongoose');
+// const cors = require('cors');
+const dotenv = require('dotenv');
+const router = require('./router');
+
+dotenv.config();
+
+const app = express();
+
+// mongoose
+//   .connect(
+//     'mongodb+srv://' +
+//       process.env.DB_USER +
+//       ':' +
+//       process.env.DB_PASSWORD +
+//       '@cluster0.ooqgv.mongodb.net/' +
+//       process.env.DB_NAME +
+//       '?retryWrites=true&w=majority',
+//     { useNewUrlParser: true, useUnifiedTopology: true }
+//   )
+//   .then(() => console.log('Connexion à MongoDB réussie !'))
+//   .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization,'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  );
+  next();
+});
+
+app.use(express.json());
+// app.use(cors());
+router(app);
+
+module.exports = app;
